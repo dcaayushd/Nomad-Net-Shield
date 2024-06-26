5615408d01e8a606a8c8215c271ff456d56f5d37
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-26 17:52:54 +0545

    Converted CSV Data to JSON Data then to Custom Dart Objects

--- lib/apis/apis.dart
+++ lib/apis/apis.dart
@@ -1,10 +1,29 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:csv/csv.dart';
 import 'package:http/http.dart';
+import 'package:nomadnetshield/models/vpn.dart';
 
 class APIs {
   static Future<void> getVpnServer() async {
+    final List<Vpn> vpnList = [];
+
     final res = await get(Uri.parse('http://www.vpngate.net/api/iphone/'));
-    log(res.body);
+    final csvString = res.body.split('#')[1].replaceAll('*', '');
+    List<List<dynamic>> list = const CsvToListConverter().convert(csvString);
+    final header = list[0];
+    for (int i = 1; i < header.length; ++i) {
+      Map<String, dynamic> tempJson = {};
+      for (int j = 0; j < header.length; ++j) {
+        tempJson.addAll(
+          {header[j].toString(): list[i][j]},
+        );
+      }
+      // log(jsonEncode(tempJson));
+      vpnList.add(Vpn.fromJson(tempJson));
+    }
+    log(vpnList.first.hostName);
+    //  log(res.body);
   }
 }
